f857904e3b3558609985c18f3eee2b6878d7972d
atharva bhandvalkar <atharv.bhandvalkar@gmail.com>
2021-11-18 09:53:14 +0530

    flappy bird mobile version

---
+++ Assets/Scripts/ColumnSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColumnSpawner : MonoBehaviour
+{
+
+    public GameObject columnPrefab;
+    
+    public float minY, maxY;
+    
+    float timer;
+    public float maxTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnColumn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer >= maxTime)
+        {
+            SpawnColumn();
+            timer = 0;
+        }
+    }
+    
+    void SpawnColumn()
+    {
+    
+        float randYPos = Random.Range(minY, maxY);
+    
+        GameObject newColumn = Instantiate(columnPrefab);
+        newColumn.transform.position = new Vector2(
+            transform.position.x,
+            randYPos);
+    }
+}
